7efe1b3dc504de90bff99a3193793faf2f356e1c
Felipe Paris <felipeparis09@gmail.com>
2023-11-18 19:29:38 -0300

    UpdateTask e DeleteTask

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,8 @@ const routes = require("express").Router();
 const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
-routes.post('/create', TaskController.createTask);
-
-
+routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
 module.exports = routes;
